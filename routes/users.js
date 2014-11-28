b6b005a8a3edd980630b7e2fd8ebdcac51bab549
lcomino <lcomino3d@gmail.com>
2014-11-28 00:21:28 -0200

    Estudando middlewere

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function(req, res, next){
+  //actions here...
+  console.log('actions');
+  next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
